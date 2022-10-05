cd889b1a976f8dff77d5b857e194149ceff12773
Ramya Ranjan Naik <48921742+rexster29@users.noreply.github.com>
2022-10-05 12:29:56 +0530

    Add files via upload
    changes to show which player wins

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
 
 var generateTwoRandomNumbers = () => {
     var randomNumber1 = Math.floor(Math.random() * 7);
-    console.log(randomNumber1);
+    //console.log(randomNumber1);
 
     var randomNumber2 = Math.floor(Math.random() * 7);
-    console.log(randomNumber2);
+    //console.log(randomNumber2);
 
     var getPlayerOneElement = document.querySelector(".dice .img1");
-    console.log(getPlayerOneElement);
+    //console.log(getPlayerOneElement);
 
     var getPlayerTwoElement = document.querySelector(".dice .img2");
-    console.log(getPlayerTwoElement);
+    //console.log(getPlayerTwoElement);
 
+    if(randomNumber1 > randomNumber2){
+        var getHeadingElement = document.querySelector(".heading");
+        getHeadingElement.textContent = "Player 1 Wins!";
+    }
+    else if(randomNumber1 < randomNumber2){
+        var getHeadingElement = document.querySelector(".heading");
+        getHeadingElement.textContent = "Player 2 Wins!";
+    }
+    
     switch (randomNumber1) {
         case 1:
             getPlayerOneElement.setAttribute("src", "images/dice1.png")
-            console.log(1);
+            //console.log(1);
             break;
         case 2:
             getPlayerOneElement.setAttribute("src", "images/dice2.png")
-            console.log(2);
+            //console.log(2);
             break;
         case 3:
             getPlayerOneElement.setAttribute("src", "images/dice3.png")
-            console.log(3);
+            //console.log(3);
             break;
         case 4:
             getPlayerOneElement.setAttribute("src", "images/dice4.png")
-            console.log(4);
+            //console.log(4);
             break;
         case 5:
             getPlayerOneElement.setAttribute("src", "images/dice5.png")
-            console.log(5);
+            //console.log(5);
             break;
         case 6:
             getPlayerOneElement.setAttribute("src", "images/dice6.png")
-            console.log(6);
+            //console.log(6);
             break;
             
     }
@@ -43,27 +52,27 @@ var generateTwoRandomNumbers = () => {
     switch (randomNumber2) {
         case 1:
             getPlayerTwoElement.setAttribute("src", "images/dice1.png")
-            console.log(1);
+            //console.log(1);
             break;
         case 2:
             getPlayerTwoElement.setAttribute("src", "images/dice2.png")
-            console.log(2);
+            //console.log(2);
             break;
         case 3:
             getPlayerTwoElement.setAttribute("src", "images/dice3.png")
-            console.log(3);
+            //console.log(3);
             break;
         case 4:
             getPlayerTwoElement.setAttribute("src", "images/dice4.png")
-            console.log(4);
+            //console.log(4);
             break;
         case 5:
             getPlayerTwoElement.setAttribute("src", "images/dice5.png")
-            console.log(5);
+            //console.log(5);
             break;
         case 6:
             getPlayerTwoElement.setAttribute("src", "images/dice6.png")
-            console.log(6);
+            //console.log(6);
             break;
     }
 }
